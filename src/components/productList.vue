e52282acdedb94d7e0087ad7848f266e2e4a778b
lishuang01 <lishuang01@gridsum.com>
2017-10-30 18:37:12 +0800

    feat: 添加http-resource

--- src/components/productList.vue
+++ src/components/productList.vue
@@ -5,7 +5,52 @@
 </template>
 
 <script>
+  export default {
+    name: 'productList',
+    created() {
+      this.getProduct();
+    },
+    data() {
+      return {
+        products: [
+          {
+            name: '首页',
+            route: 'index',
+            key: 'index',
+          },
+          {
+            name: '关于',
+            route: 'about',
+            key: 'about'
+          },
+          {
+            name: '产品列表',
+            route: 'productList',
+            key: 'productList'
+          }
+        ]
+      }
+    },
+    methods:{
+      getProduct: function () {
+        this.$http.get('http://pullword.com/biz/biz.php?weixin_id=测试').then(response => {
+          console.log(response.body);
 
+          // get body data
+          this.someData = response.body;
+
+        }, response => {
+          // error callback
+        });
+      }
+
+    },
+    watch: {
+//      '$route': function (value, oldValue) {
+//        this.currentRoute = value.name;
+//      }
+    }
+  }
 
 
 </script>
--- src/main.js
+++ src/main.js
@@ -1,12 +1,18 @@
 // The Vue build version to load with the `import` command
 // (runtime-only or standalone) has been set in webpack.base.conf with an alias.
 import Vue from 'vue';
+import VueResource from 'vue-resource';
+
 import App from './App';
 import router from './router';
 import $ from 'jquery';
 import 'bootstrap/dist/js/bootstrap.min';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import 'bootstrap/dist/css/bootstrap-theme.min.css';
+
+Vue.use(VueResource);
+// Vue.http.headers.common['Authorization'] = 'Basic YXBpOnBhc3N3b3Jk';
+
 
 Vue.config.productionTip = false;
 
@@ -15,5 +21,12 @@ new Vue({
   el: '#app',
   router,
   template: '<App/>',
-  components: { App }
+  components: { App },
+  http: {
+    root:'/',
+    headers: {
+      USERID: 'abcdefg',
+      "Content-Type": "text/html; charset=UTF-8"
+    }
+  }
 });
